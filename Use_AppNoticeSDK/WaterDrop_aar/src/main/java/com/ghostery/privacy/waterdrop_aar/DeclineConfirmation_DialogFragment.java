b247eb2931bb8604e5830fe56058cf1229b3f1da
Steven Overson <soverson@ghostery.com>
2015-10-20 10:41:11 -0600

    Add new features from FireBall into WaterDrop

--- Use_AppNoticeSDK/WaterDrop_aar/src/main/java/com/ghostery/privacy/waterdrop_aar/DeclineConfirmation_DialogFragment.java
+++ Use_AppNoticeSDK/WaterDrop_aar/src/main/java/com/ghostery/privacy/waterdrop_aar/DeclineConfirmation_DialogFragment.java
@@ -6,8 +6,6 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
-import static com.ghostery.privacy.waterdrop_aar.R.string;
-
 public class DeclineConfirmation_DialogFragment extends DialogFragment
 {
     public DeclineConfirmation_DialogFragment()
@@ -18,9 +16,9 @@ public class DeclineConfirmation_DialogFragment extends DialogFragment
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         AlertDialog.Builder builder =  new AlertDialog.Builder(getActivity());
-        builder.setTitle(getActivity().getString(string.declineConfirmDialog_title));
-        builder.setMessage(getActivity().getString(string.declineConfirmDialog_message));
-        builder.setPositiveButton(getActivity().getString(string.declineConfirmDialog_posBtn), new DialogInterface.OnClickListener() {
+        builder.setTitle(getActivity().getString(R.string.declineConfirmDialog_title));
+        builder.setMessage(getActivity().getString(R.string.declineConfirmDialog_message));
+        builder.setPositiveButton(getActivity().getString(R.string.declineConfirmDialog_posBtn), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
